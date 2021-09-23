b8085dcd30e0d4fdc9af407685acdc4aa43729ae
Ian Kahwai <iankahwai@gmail.com>
2021-09-23 11:26:04 +0200

    Update minor things

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parties</title>
+    <title>Booking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
 </head>
@@ -37,22 +37,23 @@ if(isset($_SESSION['user'])){
     else {
         ?>
             <nav>
-        <div class="px-3 py-3 flex justify-between w-full bg-white shadow-lg" >
-            <div class="flex">
-                <a href="lander.php">
-                <div class="font-bold text-lg">
-                    Venue Booking system
+                <div class="px-3 py-3 flex justify-between w-full bg-white shadow-lg" >
+                    <div class="flex">
+                        <a href="lander.php">
+                        <div class="font-bold text-lg">
+                            Venue Booking system
+                        </div>
+                        </a>
+                    </div>
+                    <div class="flex px-2 space-x-4">
+                        <a class="uppercase" href="lander.php">Home</a>
+                        <a class="uppercase" href="about.php">About</a>
+                        <a class="uppercase" href="contact.php">Contact</a>
+                        <a class="uppercase" href="login.php">Login</a>
+                        <a class="uppercase" href="signup.php">Register</a>
+                    </div>
                 </div>
-                </a>
-            </div>
-            <div class="flex px-2 space-x-4">
-                <a class="uppercase" href="about.php">About</a>
-                <a class="uppercase" href="contact.php">Contact</a>
-                <a class="uppercase" href="login.php">Login</a>
-                <a class="uppercase" href="signup.php">Register</a>
-            </div>
-        </div>
-    </nav>
+            </nav>
         <?php
     }
 ?>
